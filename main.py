5a2480d222079d4ab134853ec76e9c94684c5261
daeuni <dek233@gmail.com>
2018-07-17 23:22:01 +0100

    success node3 routing to node1

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from paramiko import SSHClient, AutoAddPolicy
 #process 3
 NO_DETECTION = "node"
 INPUT_FILE = "/home/pi/detect.txt"
-#second node side
-MINE = "2"
-ROUTING_TABLE = {'1':'3', '3':'1'}
-IP_TABLE = {'1':'192.168.1.1', '3':'192.168.1.3'}
 
-def connectToPi(ip, userName='pi', pw='1357') :
+#third node side
+MINE = "3"
+ROUTING_TABLE = {'1':2, '2':1}
+IP_TABLE = {1:'192.168.1.1', 2:'192.168.1.2'}
+
+def connectToPi(ip, username='pi', pw='1357') :
+    print('connecting to {}@{}...'.format(username,ip))
     ssh = SSHClient()
     ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.connect(ip, username=userName, password =pw)
-    print('connection status = ', ssh.get_transport().is_active())
+    ssh.connect(ip,username=username,password=pw)
+    print('connection status = ',ssh.get_transport().is_active())
     return ssh
 
 def sendCommand(ssh, command, pw='1357') :
@@ -28,7 +30,7 @@ def sendCommand(ssh, command, pw='1357') :
 # write [MINE] + from + [srcNode] in detect.txt
 def routeDetection(myssh, srcNode) :
     replaceStr = MINE + "from" +srcNode
-    cmd = "vim -c \"%s/node/"+replaceStr+"/g\" -c \"wq\" detect.txt"
+    cmd = "vi -c \"%s/node/"+replaceStr+"/g\" -c \"wq\" detect.txt"
     sendCommand(myssh, command=cmd)
     
 # check detection recursively
@@ -37,17 +39,20 @@ def routeDetection(myssh, srcNode) :
 # nodes[0] : neighbor node
 # nodes[1] : source node
 def checkDetection() :
-    f = open(INPUT_FILE,'r+')
+    f = open(INPUT_FILE,'r')
     line = f.readline()
     if line == NO_DETECTION :
         f.close()
     else :
         nodes = line.split("from")
+        f.close()
         destPi = ROUTING_TABLE[nodes[0]]
         myssh = connectToPi(ip=IP_TABLE[destPi])
         routeDetection(myssh, nodes[1])
+        f = open(INPUT_FILE,'w+')
         f.write(NO_DETECTION)
         f.close()
     #checkDetection()
 
 checkDetection()
+#myssh = connectToPi(ip='192.168.1.1')
