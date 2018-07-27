61e2994537e1d1dbe4bf9350f63ca69c422ab558
cchloe2311 <cchloe2311@daum.net>
2018-07-27 09:56:26 -0900

    done part#1 (initialization node's infomation)

--- start.py
+++ start.py
@@ -1,10 +1,11 @@
 from paramiko import SSHClient, AutoAddPolicy
 import os, sys
+from threading import Thread, Condition
 
 MINE = 1
 ipAddress = []
-INFO_PATH = '/home/pi/info.txt'
-IS_MANAGER = 0
+INFO_PATH = '/home/pi/info1.txt'
+IS_MANAGER = '0'
 
 def connectToPi(ip, username='pi', pw='1357') :
     ssh = SSHClient()
@@ -20,19 +21,26 @@ def sendCommand(ssh, command, pw='1357') :
 
 def routeInitial(myssh, text) :
     info_path = INFO_PATH.split("/home/pi/")
-    cmd = "vi -c \"%s/initail/"+text+"/g\" -c \"wq\" " + info_path[1]+"" # step 3-1 : write txt file in neibor node
+    cmd = "vi -c \"%s/initial/"+text+"/g\" -c \"wq\" " + info_path[1]+"" # step 3-1 : write txt file in neibor node
     sendCommand(myssh, command=cmd)
+    print("start other")
     cmd = "python middle_node_start.py"
+    #cmd = "python main.py" # for test
     sendCommand(myssh, command=cmd) # step 3-2 : run middle_node_start.py to route input to neighbor's neighbor
+    #startOtherNodeThread = StartOtherNodeThread()
+    #thread = threading.Thread(target=startOtherNodeThread.run)
+    #thread.start()
     
 def adHocNetwork(dest, text) :
     myssh = connectToPi(ip=dest)
+    print(myssh)
     routeInitial(myssh, text)
     
 def isManager(managerList) :
     managers = managerList.split(" ")
-    for i in range(0, length(managers)) :
-        if(int(managers[i]) == MINE) return '1'
+    for i in range(0, len(managers)) :
+        if(int(managers[i]) == MINE):
+            return '1'
     return '0'
 
 ## code starts ##
@@ -47,9 +55,11 @@ for i in range(0, int(numOfNode)) :
     ipAddress.append(tempIp)
     text += "\n" + tempIp # step 1 : get users input
     
-# get manager node's id until user click enter ******
+# get manager node's id until user click enter
 managerList = input()
+print("manager list : " + managerList)
 IS_MANAGER = isManager(managerList)
+print("IS_MANAGER : " + IS_MANAGER)
 
 f = open(INFO_PATH,'w+')
 f.write(text) # step 2 : write inputs in file in its node
@@ -58,5 +68,6 @@ f.close()
 adHocNetwork(ipAddress[1], text) # step 3 : route inputs to other nodes
 
 cmd = "python main.py " + IS_MANAGER # step 4 : start main.py
+#cmd = "python pyaudioPlayer.py" # for test
 os.system(cmd)
-print("done")
+print("program done")
